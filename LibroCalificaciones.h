b69f378126e6866cbbb687ff04340c543255395a
NoirRM907A <53484018+NoirRM907A@users.noreply.github.com>
2019-08-06 09:41:50 -0600

    Add files via upload

--- LibroCalificaciones.h
+++ LibroCalificaciones.h
@@ -1,26 +1,85 @@
-/*
- * =====================================================================================
- *
- *       Filename:  LibroCalificaciones.h
- *
- *    Description:  Encabezado de la clase Libro Calificaciones
- *
- *        Created:  2019-08-05
- *
- *         Author:  Maikol Guzman Alan mikeguzman@gmail.com
- *   Organization:  Universidad Nacional de Costa Rica
- *
- * =====================================================================================
- */
+// Alumno: Noel Mu;oz Ramirez
+// Universidad Nacional
+// Profesor: Maikol Gusman
+// 06/08/2019
 
 
-#ifndef LIBROCALIFICACIONES_LIBROCALIFICACIONES_H
-#define LIBROCALIFICACIONES_LIBROCALIFICACIONES_H
 
+
+
+#ifndef LIBROCALIFICACIONES_H
+#define LIBROCALIFICACIONES_H
+#include "LibroCalificaciones.cpp"
+#include<iostream>
+#include<sstream>
+using namespace std;
 
 class LibroCalificaciones {
+private:  
+	string nomCur;
+	int nota;
+	string ontenerRangoLetra();
+	
+public:
+	LibroCalificaciones(){
+		nomCur=" ";
+		nota=0;
+	}
 
+	LibroCalificaciones(string nC, int n){
+		nomCur=nC;
+		nota=n;
+	}
+    ~LibroCalificaciones(){}	
+	
+	void setNomCur(string nC){
+		nomCur=nC;
+	}
+	
+	void setNota(int n){
+		nota=n;
+	}
+	
+	string getNomCur(){
+		return nomCur;
+	}
+	
+	int getNota(){
+		return nota;
+	}
+	
+///////////////////////////////////////	
+	
+	string obteneRangoLetra(){
+		if(nota<=90){
+			return "-A-";
+		}else{
+			if(nota<=80){
+				return "-B-";
+			}else{
+				if(nota<=70){
+					return "-C-";
+				}else{
+					if(nota<=60){
+						return "-D-";
+					}else{
+						return "-F-";
+					}
+				}
+			}
+		}
+	}
+	
+	/*string obtenerMensaje(){
+		if(obtenerRangoLetra()=="-A-"){
+			return 
+		}
+		
+		
+		
+	}*/
+	
 };
 
+#endif
 
-#endif //LIBROCALIFICACIONES_LIBROCALIFICACIONES_H
--- main.cpp
+++ main.cpp
@@ -1,19 +1,12 @@
-/*
- * =====================================================================================
- *
- *       Filename:  main.cpp
- *
- *    Description:  Main Libro Calificaciones
- *
- *        Created:  2019-08-05
- *
- *         Author:  Maikol Guzman Alan mikeguzman@gmail.com
- *   Organization:  Universidad Nacional de Costa Rica
- *
- * =====================================================================================
- */
+#include<iostream>
+#include "LibroCalificaciones.h"
+using namespace std;
 
-int main() {
-
-    return 0;
+int main (int argc, char *argv[]) {
+	LibroCalificaciones Lc1("Al", 88);
+	
+	Lc1.obteneRangoLetra();
+	
+	return 0;
 }
+
